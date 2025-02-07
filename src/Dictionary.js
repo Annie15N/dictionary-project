4dcbdbf35d632c918133410f8399c305f90314f1
Annie15N <annie.nelson158@gmail.com>
2025-02-07 16:17:26 +0000

    Finished API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,14 +6,15 @@ import "./Dictionary.css";
 export default function Dictionary() {
   let [word, setWord] = useState(null);
 
-  function handleResult(response) {}
+  function handleResult(response) {
+    console.log(response.data.meanings[0]);
+  }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${word}`);
 
     let apiKey = "0cod3f6efbetca0af47e36320cff442c";
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word={word}&key=${apiKey}`;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResult);
   }
 
